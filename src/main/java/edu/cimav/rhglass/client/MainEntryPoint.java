6a92487992d8fba43940091e88152fe3a8caf102
juan.calderon <juan.calderon@LenovoCalderon.cimav.cimav.edu.mx>
2015-02-20 16:28:51 -0700

    Add Main class.

--- src/main/java/edu/cimav/rhglass/client/MainEntryPoint.java
+++ src/main/java/edu/cimav/rhglass/client/MainEntryPoint.java
@@ -5,13 +5,13 @@
  */
 package edu.cimav.rhglass.client;
 
-import com.google.gwt.core.client.Duration;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.shared.UmbrellaException;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import edu.cimav.rhglass.client.ui.main.MainUI;
+import javax.validation.constraints.NotNull;
+import org.fusesource.restygwt.client.Defaults;
 
 /**
  * Main entry point.
@@ -20,33 +20,85 @@ import com.google.gwt.event.dom.client.ClickHandler;
  */
 public class MainEntryPoint implements EntryPoint {
 
+    static {
+        /* Default initialize */
+        Defaults.setServiceRoot(GWT.getHostPageBaseURL());
+        Defaults.ignoreJsonNulls();
+        String DATEFORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
+        Defaults.setDateFormat(DATEFORMAT); //2015-01-14T00:00:00-0700
+    }
+    
+    // Vistas UI
+    private final MainUI mainUi;
+    
     /**
      * Creates a new instance of MainEntryPoint
      */
     public MainEntryPoint() {
+        
+        mainUi = new MainUI();
+        
+        GWT.setUncaughtExceptionHandler(new GWT.UncaughtExceptionHandler() {
+            @Override
+            public void onUncaughtException(@NotNull Throwable e) {
+                ensureNotUmbrellaError(e);
+            }
+        });
+        
     }
 
-    private boolean isCyan = true;
+//    private boolean isCyan = true;
     
     /**
      * The entry point method, called automatically by loading a module that declares an implementing class as an entry-point
      */
     @Override
     public void onModuleLoad() {
-        final Label label = new Label("Hello, GWT!!!");
-        final Button button = new Button("Click me!");
         
-        button.addClickHandler(new ClickHandler() {
+        RootLayoutPanel.get().add(mainUi);
+
+        mainUi.addOptionMenuChangeListener(new MainUI.OptionMenuChangeListener() {
             @Override
-            public void onClick(ClickEvent event) {
-                String str = " " + (Duration.currentTimeMillis() * 100000) + " <<<<<< ";
-                label.setText(str);
-                String color = (isCyan = !isCyan) ? "lightblue" : "lightsalmon";
-                label.getElement().getStyle().setBackgroundColor(color);
+            public void onOptionMenuChange(String option) {
+                switch (option) {
+                    case MainUI.OPT_PERSONAL:
+                        mainUi.setCenterPanel("Personal", "Consultas, altas, bajas y cambios", null);
+                        break;
+                    case MainUI.OPT_DEPARTAMENTOS:
+                        mainUi.setCenterPanel("Departamentos", "Consultas, altas, bajas y cambios", null);
+                        break;
+                    default:
+                        mainUi.setCenterPanel(option, "Not Yet Implemented...", null);
+                        break;
+                }
             }
         });
         
-        RootPanel.get().add(button);
-        RootPanel.get().add(label);
+//        final Label label = new Label("Hello, GWT!!!");
+//        final Button button = new Button("Click me!");
+//        
+//        button.addClickHandler(new ClickHandler() {
+//            @Override
+//            public void onClick(ClickEvent event) {
+//                String str = " " + (Duration.currentTimeMillis() * 100000) + " <<<<<< ";
+//                label.setText(str);
+//                String color = (isCyan = !isCyan) ? "lightblue" : "lightsalmon";
+//                label.getElement().getStyle().setBackgroundColor(color);
+//            }
+//        });
+//        
+//        RootPanel.get().add(button);
+//        RootPanel.get().add(label);
     }
+    
+    private static void ensureNotUmbrellaError(@NotNull Throwable e) {
+        for (Throwable th : ((UmbrellaException) e).getCauses()) {
+            if (th instanceof UmbrellaException) {
+                ensureNotUmbrellaError(th);
+            } else {
+                System.err.println(th);
+            }
+        }
+    }
+    
 }
---
+++ src/main/java/edu/cimav/rhglass/client/ui/main/MainUI.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cimav.rhglass.client.ui.main;
+
+import com.github.gwtbootstrap.client.ui.NavLink;
+import com.github.gwtbootstrap.client.ui.NavList;
+import com.github.gwtbootstrap.client.ui.base.IconAnchor;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.StackLayoutPanel;
+import com.google.gwt.user.client.ui.Widget;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author juan.calderon
+ */
+public class MainUI extends Composite {
+
+    private static final MainUiBinder uiBinder = GWT.create(MainUiBinder.class);
+
+    interface MainUiBinder extends UiBinder<Widget, MainUI> {
+    }
+
+    public static final String OPT_NONE = "optionNone";
+    public static final String OPT_PERSONAL = "optionPersonal";
+    public static final String OPT_DEPARTAMENTOS = "optionDepartamentos";
+    public static final String OPT_TABULADOR = "optionTabulador";
+
+    @UiField StackLayoutPanel westPanel;
+    @UiField FlowPanel workPanel;
+    @UiField Label lTitulo;
+    @UiField Label lSubTitulo;
+
+    private Widget currentWorkWidget;
+
+    public void setTittulo(String t) {
+        lSubTitulo.setText(t);
+    }
+
+    @UiHandler({"optionUno", "optionDos", "optionTres", OPT_PERSONAL, OPT_DEPARTAMENTOS, OPT_TABULADOR})
+    protected void onClick(ClickEvent e) {
+
+        // Pone todos los sub-menus en DesActivados
+        Iterator<Widget> arrayOfWidgets = westPanel.iterator();
+        while (arrayOfWidgets.hasNext()) {
+            Widget widgetNavList = arrayOfWidgets.next();
+            if (widgetNavList instanceof NavList) {
+                NavList navList = (NavList) widgetNavList;
+                for (int i = 0; i < navList.getWidgetCount(); i++) {
+                    Widget widgetNavLink = navList.getWidget(i);
+                    if (widgetNavLink instanceof NavLink) {
+                        NavLink navLink = (NavLink) widgetNavLink;
+                        navLink.setActive(false);
+                    }
+                }
+            }
+        }
+        // Activa el clickeado
+        IconAnchor src = (IconAnchor) e.getSource();
+        NavLink navLink = (NavLink) src.getParent();
+        navLink.setActive(true);
+
+        // just to know...
+        onOptionMenuChange(navLink.getName());
+        if (OPT_PERSONAL.equals(navLink.getName())) {
+        } else {
+        }
+    }
+
+    public MainUI() {
+        initWidget(uiBinder.createAndBindUi(this));
+
+        westPanel.getElement().setAttribute("id", "west-panel");
+    }
+
+    public void setCenterPanel(String titulo, String subTitulo, Widget widget) {
+
+        // remover el widget anterior (si habia)
+        if (currentWorkWidget != null && workPanel.getWidgetIndex(currentWorkWidget) >= 0) {
+            workPanel.remove(currentWorkWidget);
+        }
+        //workPanel.clear(); // remover lo anterior
+
+        // keep el widget nuevo
+        currentWorkWidget = widget;
+
+        lTitulo.setText(titulo);
+        lSubTitulo.setText(subTitulo);
+
+        if (currentWorkWidget != null) {
+            workPanel.add(currentWorkWidget); // agregar el enviado
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="interface OptionMenuChangeListener para cuando selecciona una Option"> 
+    public interface OptionMenuChangeListener extends java.util.EventListener {
+
+        void onOptionMenuChange(String option);
+    }
+    private final ArrayList listeners = new ArrayList();
+
+    public void addOptionMenuChangeListener(OptionMenuChangeListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeOptionMenuChangeListener(OptionMenuChangeListener listener) {
+        listeners.remove(listener);
+    }
+
+    public void onOptionMenuChange(String option) {
+        for (Iterator it = listeners.iterator(); it.hasNext();) {
+            OptionMenuChangeListener listener = (OptionMenuChangeListener) it.next();
+            listener.onOptionMenuChange(option);
+        }
+    }
+    // </editor-fold>
+
+}
